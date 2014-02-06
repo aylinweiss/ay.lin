5e49598a2a9f2e82c5567033e07190ca4ad7db28
aylinweiss <aylin.weiss.AW@googlemail.com>
2014-02-06 17:29:12 +0100

    final

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ActiveRecord::Base
 	validates :name, presence: true
 	validates :prise, presence: true
+	validates :image, presence: true
+
 	belongs_to :user
 	mount_uploader :image, ImageUploader
 end
